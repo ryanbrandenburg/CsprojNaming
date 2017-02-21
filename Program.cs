390d9e2685a2104eb77add47f7b1baaa5c6395e2
Ryan Brandenburg <rybrande@microsoft.com>
2017-02-21 13:06:29 -0800

    Initial comit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Resources;
+
+namespace Assembly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var manager = new ResourceManager(typeof(Program));
+
+            CultureInfo.CurrentCulture = new CultureInfo("sv");
+            CultureInfo.CurrentUICulture = new CultureInfo("sv");
+
+            Console.WriteLine(manager.GetString("Test"));
+            Console.ReadLine();
+        }
+    }
+}
